27ac1e06754e5517863174471d2432ee5ca27503
asrock <asrock@DESKTOP-FDOSO53>
2023-12-01 13:24:09 +0200

    123

--- src/Algs/MergeSort2.java
+++ src/Algs/MergeSort2.java
@@ -10,45 +10,9 @@ public class MergeSort2 {
 
 	public static List<String> sort(List<String> l) {
 		l1.addAll(l);
-		parts(l);
+		parts1(l);
 		// parts1(l);
 		return l1;
-	}
-
-	private static void parts(List<String> l) {
-		partSort(l, 0, l.size());
-	}
-
-	private static void partSort(List<String> l, int start, int size) {
-		int max_part = (int) Math.pow(2, ((int) (Math.log(l.size()) / Math.log(2))));
-		int remain = l.size() - max_part;
-		System.out.println("size: " + l.size() + ", max_part: " + max_part + ", last: " + remain);
-		//
-		int part = 1, parts, last;
-		int s1 = start, s2 = s1 + part;
-		int sz1, sz2;
-		while (part < size) {
-			parts = size / part;
-			last = size % part;
-			System.out.println("size: " + size + ", parts: " + parts + ", part: " + part + ", last: " + last);
-			for (int i = 0; i < parts / 2; i++) {
-				s1 = part * i * 2;
-				s2 = s1 + part;
-				sz1 = sz2 = part;
-				mergeSort(l, s1, sz1, s2, sz2);
-			}
-			if (parts == 1 && last == 1) {
-				System.out.println("parts == 1 && last == 1");
-				mergeSort(l, 0, part, size - 1, 1);
-			}
-			// l = l1; // both variables point to the same object !!! 3 days bug !!!
-			l.clear(); // empty and replace the values with the processed ones
-			l.addAll(l1);
-			part *= 2;
-		}
-		if (remain > 1) {
-			//partSort(l,max_part, remain);
-		}
 	}
 
 	private static void parts1(List<String> l) {
@@ -65,9 +29,25 @@ public class MergeSort2 {
 				sz1 = sz2 = part;
 				mergeSort(l, s1, sz1, s2, sz2);
 			}
+			if (last % 2 != 0 && last > 1) { // sort odd size partitions
+				s1 = l.size() - last;
+				sz1 = last - last % 2;
+				s2 = l.size() - last % 2;
+				sz2 = last % 2;
+				// System.out.println(s1 + ", " + sz1 + ", " + s2 + ", " + sz2);
+				print_sub("last: ", l, s1, sz1, s2, sz2);
+				mergeSort(l, s1, sz1, s2, sz2);
+			}
+			if (parts == 1) { // largest part + remainder
+				s1 = 0;
+				sz1 = part;
+				s2 = l.size() - last;
+				// print_sub("last: ", l, s1, sz1, s2, last);
+				mergeSort(l, s1, sz1, s2, last);
+			}
 			// l = l1; // both variables point to the same object !!! 3 days bug !!!
-			l.clear(); // empty and replace the values with the processed ones
-			l.addAll(l1);
+			// l.clear(); // empty and replace the values with the processed ones
+			// l.addAll(l1);
 			part *= 2;
 		}
 	}
@@ -89,6 +69,8 @@ public class MergeSort2 {
 			l1.set(k++, l.get(j++));
 		}
 		// print_sub("after: ", l1, s1, sz1, s2, sz2);
+		l.clear(); // empty and replace the values with the processed ones
+		l.addAll(l1);
 	}
 
 	private static void print_sub(String msg, List<String> l, int s1, int sz1, int s2, int sz2) {
@@ -96,6 +78,7 @@ public class MergeSort2 {
 		for (int i = s1; i < s1 + sz1; i++) {
 			out += l.get(i) + ", ";
 		}
+		System.out.println(out);
 		out = out.substring(0, out.length() - 2) + "), (";
 		for (int i = s2; i < s2 + sz2; i++) {
 			out += l.get(i) + ", ";
