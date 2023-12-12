d146171911eb7f10bb8aff04e6c25fc5fd16e93c
asrock <asrock@DESKTOP-FDOSO53>
2023-12-12 16:20:38 +0200

    123

--- src/Algs/QuickSort.java
+++ src/Algs/QuickSort.java
@@ -7,64 +7,56 @@ public class QuickSort {
 
 	public static List<String> sort(List<String> l) {
 		if (l.size() < 3) {
-			System.out.println("array size must be >= 3");
-			return l;
+			// System.out.println("array size must be >= 3");
+			// return l;
 		}
-		boolean swap = false;
-		int mid = pivot(l);
-		System.out.println("mid: " + l.get(mid));
-		int lo = 0, hi = l.size() - 1;
-		while (lo != mid && hi != mid) {
-			lo = scan_lo(l, mid, lo);
-			//System.out.println("scan lo: " + l.get(lo));
-			hi = scan_hi(l, mid, hi);
-			//System.out.println("scan hi: " + l.get(hi));
-			if (lo != mid && hi != mid) {
-				swap = swap(l, lo, hi);
+		return quickSort(l, 0, l.size());
+	}
+
+	private static List<String> quickSort(List<String> l, int s, int e) {
+		int p = pivot(l, s, e);
+		// int p = e - 1; // pivot is always the last
+		int i = -1, j = 0;
+		System.out.println("p: " + l.get(p) + "; s: " + s + "; e: " + e);
+		while (j < e) {
+			_LoadData.count++;
+			// if j > p: j++
+			if (Integer.parseInt(l.get(j)) > Integer.parseInt(l.get(p))) {
+				j++;
 			}
+			// if j < p: i++, swap i & j val
+			if (Integer.parseInt(l.get(j)) < Integer.parseInt(l.get(p))) {
+				swap(l, ++i, j);
+			}
+			// when j reach p: i++, swap i & p val
+			if (j == p) {
+				swap(l, ++i, p);
+			}
+			j++;
 		}
-		// _LoadData.Print("interim > ", l);
-		if (swap) { // recursion
-			sort(l);
+		// recursion call
+		if (e - s > 1) {
+			quickSort(l, s, s + (e - s) / 2);
+			quickSort(l, s + (e - s) / 2, e);
 		}
 		return l;
 	}
 
-	private static int pivot(List<String> l) {
-		// int mid = ThreadLocalRandom.current().nextInt(1, l.size() - 1);
-		int mid = l.size() / 2, lo = 0, hi = l.size() - 1;
-		if (Integer.parseInt(l.get(hi)) < Integer.parseInt(l.get(lo))) {
-			swap(l, lo, hi);
-		}
-		if (Integer.parseInt(l.get(mid)) < Integer.parseInt(l.get(lo))) {
-			swap(l, mid, lo);
-		}
-		if (Integer.parseInt(l.get(mid)) > Integer.parseInt(l.get(hi))) {
-			swap(l, mid, hi);
-		}
-		return mid;
-	}
-
-	private static int scan_lo(List<String> l, int mid, int lo) {
-		for (int i = lo; i < mid; i++) {
-			_LoadData.count++;
-			if (Integer.parseInt(l.get(mid)) < Integer.parseInt(l.get(i)))
-				return i;
-		}
-		return mid;
-	}
-
-	private static int scan_hi(List<String> l, int mid, int hi) {
-		for (int i = hi; i > mid; i--) {
-			_LoadData.count++;
-			if (Integer.parseInt(l.get(mid)) > Integer.parseInt(l.get(i)))
-				return i;
-		}
-		return mid;
+	private static int pivot(List<String> l, int s, int e) { // median of three
+		int p1 = s;
+		int p2 = (e - s) / 2;
+		int p3 = e - 1;
+		if (Integer.parseInt(l.get(p1)) > Integer.parseInt(l.get(p2)))
+			swap(l, p1, p2);
+		if (Integer.parseInt(l.get(p2)) < Integer.parseInt(l.get(p3)))
+			swap(l, p2, p3);
+		if (Integer.parseInt(l.get(p1)) > Integer.parseInt(l.get(p3)))
+			swap(l, p1, p3);
+		return p3;
 	}
 
 	private static boolean swap(List<String> l, int lo, int hi) {
-		System.out.println("SWAP > lo: " + l.get(lo) + ", hi: " + l.get(hi));
+		// System.out.println("SWAP > lo: " + l.get(lo) + ", hi: " + l.get(hi));
 		String temp = l.get(hi);
 		l.set(hi, l.get(lo));
 		l.set(lo, temp);
